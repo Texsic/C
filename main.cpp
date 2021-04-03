37a0362bac69c081b2a5b871de9ba55391c8626e
Texsic <81568601+Texsic@users.noreply.github.com>
2021-04-03 13:20:38 +0300

    Hello World

--- main.cpp
+++ main.cpp
@@ -1,20 +1,13 @@
+// Hello World
+// Test Program
+
 #include <iostream>
-#include <iomanip>
+
 using namespace std;
 
-int main()
-{
-    float fValue = 76.4;
-    cout << "Size of float: " << sizeof(float) << endl;
-    cout << fixed << fValue << endl;
-    cout << scientific << fValue << endl;
-    cout << setprecision(20) << fixed << fValue << endl;
+int main() {
+cout << "Hello World :D" << endl;
 
-    double dValue = 76.4;
-    cout << setprecision(20) << fixed << dValue << endl;
+return 0;
 
-    long double lValue = 123.456789876543210;
-    cout << setprecision(20) << fixed << lValue << endl;
-    cout << "Size of long double: " << sizeof(lValue) << endl;
-    return 0;
 }
